78aa58aa6c221c4686a35f3aa59761387cdf5a6d
Catarina Julio <Catarinajulioo@gmail.com>
2022-11-06 00:56:02 +0200

    thirst commit

--- ierc.sol
+++ ierc.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.0;
  
 interface IERC20{
-    function totalSupply() external view returns(uint);
-    function balanceOf(address account) external view returns(uint);
-    function transfer(address recipient, uint amount) external returns(bool);
-    function allowance(address owner, address spender) external view returns(uint);
-    function approve(address spender, uint amount) external returns(bool);
-    function transferFrom(address sender, address recipient, uint amount) external returns(bool);
+    function totalSupply() external view returns(uint); //A method that defines the total supply of your tokens, When this limit is reached the smart contract will refuse to create new tokens.
+    function balanceOf(address account) external view returns(uint); //A method that returns the number of tokens a wallet address has.
+    function transfer(address recipient, uint amount) external returns(bool); //A method that takes a certain amount of tokens from the total supply and gives it to a user.
+    function allowance(address owner, address spender) external view returns(uint); //Another type of transfer method which is used to transfer tokens between users.
+    function approve(address spender, uint amount) external returns(bool); //This method verifies whether a smart contract is allowed to allocate a certain amount of tokens to a user, considering the total supply.
+    function transferFrom(address sender, address recipient, uint amount) external returns(bool); //Another type of transfer method which is used to transfer tokens between users.
  
-event Transfer(address indexed from, address indexed to, uint amount);
+event Transfer(address indexed from, address indexed to, uint amount); //This event should be triggered when tokens are transferred from one account to another.
 event Approval(address indexed owner, address indexed spender, uint amount);
  
 }
--- main.sol
+++ main.sol
@@ -3,34 +3,36 @@ pragma solidity ^0.8.0;
  
 import "./ierc.sol";
  
-contract ERC20 is IERC20{
+contract ERC20 is IERC20{ // to use the functions of the IERC20 interface
  
-uint public override totalSupply; 
-mapping(address => uint) public override balanceOf;
-mapping(address => mapping(address => uint)) public override allowance;
-string public name ="Mike Token";
-string public symbol ="MKT";
-uint public decimals = 18;
- 
+uint public override totalSupply; // declaracao da variavel totalsupply
+mapping(address => uint) public override balanceOf; //bind unique Ethereum address to value of type uint
+mapping(address => mapping(address => uint)) public override allowance; 
+string public name ="Mike Token"; //Declaring variables and assigning the token name
+string public symbol ="MKT";//Declaring variables and assigning the token symbol
+uint public decimals = 18;//declaration of variables and assignment of the decimal precision of the token.
+
+// Function transfer which will execute the transfer of tokens from the total supply to users.
 function transfer(address recipient, uint amount) external override returns(bool){
-balanceOf[msg.sender] -= amount; //method 1 is deducting the amount from sender's balance
-balanceOf[recipient] += amount; //method 2 is adding amount to recipient's balance
+balanceOf[msg.sender] -= amount; // deduct the amount from sender's balance
+balanceOf[recipient] += amount; // add amount to recipient's balance
  
-emit Transfer(msg.sender, recipient, amount);
-return true;
+emit Transfer(msg.sender, recipient, amount); // issues the transaction, the address of the owner of the contract, the address where the amount goes and the amount.
+return true; //returns true when the transaction is performed
 }
- 
+
+//Function approve which will check if the total supply has the amount of token which needs to be allocated to a user. 
 function approve(address spender, uint amount) external override returns(bool){
-allowance[msg.sender][spender] = amount;
-emit Approval(msg.sender, spender, amount);
+allowance[msg.sender][spender] = amount; // "msg.sender" calls the token approve function to allow spender to spend a value on its behalf.
+emit Approval(msg.sender, spender, amount); // issue the function ok 
 return true;
 }
- 
+//Function transferFrom which will facilitate the transfer of token between users 
 function transferFrom(address sender, address recipient, uint amount) external override returns(bool){
-allowance[sender][msg.sender] -= amount; 
-balanceOf[sender] -= amount;
-balanceOf[recipient] += amount;
-emit Transfer(sender, recipient, amount);
+allowance[sender][msg.sender] -= amount; //"spender" calls the token approve function to allow msg.sender to deduct a value on its behalf.
+balanceOf[sender] -= amount; // deduct the amount from sender's balance
+balanceOf[recipient] += amount;// add amount to recipient's balance
+emit Transfer(sender, recipient, amount);// issues the transaction, the address of the owner of the contract, the address where the amount goes and the amount.
 return true;
 }
  
